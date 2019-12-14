3654816cb0cf2464e092b12837f01177696c2afd
Evgenii Efimchik <Evgenii_Efimchik@epam.com>
2019-12-14 13:18:34 +0300

    Spring Boot App Initial Commit with Hello World mapping

---
+++ src/main/java/com/epam/demopetproject/DemoPetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.epam.demopetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPetProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/demopetproject/DemoPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.demopetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
